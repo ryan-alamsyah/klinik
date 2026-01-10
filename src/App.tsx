e57970e2e8cc1b2e384d15e25ef6a47cedf850d8
ryanbrembo <ryanalamsyah39@gmail.com>
2026-01-10 23:11:02 +0700

    update form

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-
-import './index.css'
-import Layout from './components/Layout/Layout'
+import "./index.css";
+import Layout from "./components/Layout/Layout";
 import { Routes, Route } from "react-router";
-import DashboardPage from './Pages/DashboardPage/DashboardPage';
-import AntrianPendaftaranPage from './Pages/Antrian/AntrianPendaftaranPage';
-import DataPasienPage from './Pages/Pasien/DataPasienPage';
-import TombolAntrean from './Pages/Antrian/TombolAntrian';
-
-
+import DashboardPage from "./Pages/DashboardPage/DashboardPage";
+import AntrianPendaftaranPage from "./Pages/Antrian/AntrianPendaftaranPage";
+import DataPasienPage from "./Pages/Pasien/DataPasienPage";
+import TombolAntrean from "./Pages/Antrian/TombolAntrian";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-         
-          
-      <Route index element={<Layout />} />
-   
+
+          <Route index element={<Layout />} />
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/nomor-antrean" element={<AntrianPendaftaranPage />} />
           <Route path="/list-pasien" element={<DataPasienPage />} />
